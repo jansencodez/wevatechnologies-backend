86f0b8f1323f5d6678000328f740884f3d1abb80
jansencodez <nyamwecliff@gmail.com>
2024-12-17 09:07:03 +0300

    first commit

--- app/sec.py
+++
@@ -1,6 +0,0 @@
-import secrets
-
-# Generate a secure random secret key of 64 bytes (or adjust length as needed)
-secret_key = secrets.token_urlsafe(64)
-
-print(f"Your generated JWT_SECRET_KEY: {secret_key}")
